88f318df6e7c42b26f24c3a6a7def4500e931428
Rodgers Ogada <ogadarodgers@gmail.com>
2024-07-15 11:58:22 +0300

    html file structure and grabbing our elements from the DOM

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // const BASE_URL = 'https://new-todo-json-server-vercel.vercel.app/todos/';
 const BASE_URL = 'http://localhost:8001/todos';
+
+const createForm = document.getElementById('create-form'); // grab our form for creating new todo
+const createTitleInput = document.getElementById('create-title'); // input field for entering todo title
+const updateForm = document.getElementById('update-form'); // grab our form for updating todo
+const updateTitleInput = document.getElementById('update-title'); // input field for updating todo title
+const todosSection = document.getElementById('todos-section'); // section for displaying all todos
+const singleTodoSection = document.getElementById('single-todo-section'); // section for displaying single todo item
+
